d25b0efd0909a060a10ca5c868dfdcf5a6f226e8
Jake Montgomery <jmont96@gmail.com>
2024-03-05 12:32:47 -0600

    template

--- src/checkout/Checkout.tsx
+++ src/checkout/Checkout.tsx
@@ -1,6 +1,32 @@
-import {CoinflowPurchase} from "@coinflowlabs/react";
+import { CoinflowPurchase } from "@coinflowlabs/react";
+import { useCallback } from "react";
+import { useWallet } from "../hooks/useWallet.tsx";
+import { APP_THEME } from "../App.tsx";
+import { useHeightHandler } from "../hooks/useHeightHandler.tsx";
 
+export function Checkout() {
+  const { height, handleHeightChange } = useHeightHandler();
+  const wallet = useWallet();
 
-export function Checkout(){
-    return <CoinflowPurchase wallet={""} blockchain={""} merchantId={""} />
+  const onSuccess = useCallback(() => {
+    // Optional - TODO
+  }, []);
+
+  return (
+    <div style={{ height }}>
+      <CoinflowPurchase
+        amount={undefined}
+        transaction={undefined}
+        wallet={wallet}
+        merchantId={"coinflow"} // TODO
+        env={"sandbox"}
+        onSuccess={onSuccess}
+        theme={APP_THEME}
+        blockchain={"solana"} //TODO
+        email={""}
+        loaderBackground={APP_THEME.background}
+        handleHeightChange={handleHeightChange}
+      />
+    </div>
+  );
 }
